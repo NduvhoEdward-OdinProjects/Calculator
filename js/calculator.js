bac004b4d08f68ee0fc0380f0666d4f58befdb26
NduvhoEdward <71088485+NduvhoEdward@users.noreply.github.com>
2023-03-11 22:49:03 +0200

    Make 'add' work with more than 2 nums

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,7 @@
-function add(a,b) {
-	return a+b;
+function add(nums) {
+	return nums.reduce((currentSum, num) => {
+		return currentSum + num;
+	}, 0);
 }
 
-console.log(add(1,23));
+console.log(add([1,23]));
