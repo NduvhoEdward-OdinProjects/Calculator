bb015bff0ee1cba13232ee43103bdecdb29abdbf
NduvhoEdward <71088485+NduvhoEdward@users.noreply.github.com>
2023-03-12 20:03:44 +0200

    Add 'clear' functionality

--- js/calculator.js
+++ js/calculator.js
@@ -18,9 +18,41 @@ const operate = function(operator, a, b) {
 }
 
 let displayValue = '';
+const operators = [" + ",
+				   " - ",
+				   " / ",
+				   " x "];
+let operands = [];
+let usedOps = [];
+let tempOperand = ''; 
+let tempOp = ''; 
+
+function isOperator(event) {
+	return operators.some(op => op==event.target.id)
+}
+
 function updateDisplay(event) {
-	console.log(event);
-	displayValue += event.target.id; 
+	if (!isOperator){
+		tempOperand += event.target.id
+	} else {
+		operands.push(tempOperand);
+		tempOperand = '';
+		tempOp = event.target.id;
+		usedOps.push(tempOp);
+	} 
+	if (event.target.id=='clc'){
+		displayValue = '';
+		operands = [];
+		usedOps = [];
+		display.textContent = displayValue;
+		return;
+	}
+	
+	for (let i = 0; i < operands.length; i++) {
+		//displayValue = '';
+		displayValue += operands[i]; 
+		displayValue += usedOps[i]; 
+	}
 	display.textContent = displayValue; 
 }
 
