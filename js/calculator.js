41ad63623021d9fe524e0dd65fc16bee6ffe930e
NduvhoEdward <71088485+NduvhoEdward@users.noreply.github.com>
2023-03-14 15:26:17 +0200

    Add keyboard support

--- js/calculator.js
+++ js/calculator.js
@@ -24,16 +24,16 @@ let operand2 = '';
 let currentAnswer = 0; 
 let isFirstNum = true;
 const ops = {
-	" + ": add, 
-	" - ": subtract, 
-	" / ": divide, 
-	" * ": multiply, 
-	" = ": 'equals',
+	"+": add, 
+	"-": subtract, 
+	"/": divide, 
+	"*": multiply, 
+	"=": 'equals',
 };
 
 function isOperator(event) { 
-	const operators = [" + ", " - ", " / ", " * ", " = "]; 
-	return operators.includes(event.target.id)
+	const operators = ["+", "-", "/", "*", "="]; 
+	return operators.includes(event.target.id) 
 }
 function isNumber(event) { 
 	const numbers = ['0','1','2','3','4','5','6','7','8','9','.'];
@@ -42,7 +42,7 @@ function isNumber(event) {
 
 function update(event) {
 	if (isOperator(event)) { 
-		let currentOp = event.target.id;
+		let currentOp = event.target.id; 
 		let operator = ops[currentOp]; 
 		if(isFirstNum){
 			operand1 = operand2;
@@ -51,11 +51,15 @@ function update(event) {
 			operand2 = '';
 			return;
 		} 
+		// If a user wants to change operator
 		if (operand2=='') {
 			prevOp = ops[currentOp];
 			return;
 		}
-		if (currentOp==' = ') {
+		if (currentOp=='=') { 
+			// If not enough operands have been entered before '='
+			if (prevOp === 'equals') return; 
+			
 			currentAnswer = operate(prevOp, parseFloat(operand1),parseFloat(operand2));
 			currentAnswer = parseFloat(currentAnswer.toFixed(10));
 			operand1 = currentAnswer; 
@@ -80,10 +84,10 @@ function update(event) {
 		operand2 = '';
 		currentAnswer = 0; 
 		isFirstNum = true;
-	} else if (event.target.id==' % ') {
+	} else if (event.target.id=='%') {
 		operand2 /= 100; 
 		displayValue = operand2;
-	} else if (event.target.id==' +/- ') {
+	} else if (event.target.id=='+/-') {
 		operand2 *= -1; 
 		displayValue = operand2;	
 	} else if (event.target.id=='del') {
@@ -101,3 +105,13 @@ buttons.forEach(button => {
 	button.addEventListener('click', update);
 })
 
+function handleKeyPress(event) {
+  const key = event.key;
+  const element = document.getElementById(key);
+  
+  if (element) {
+    element.click();
+  }
+}
+
+document.addEventListener('keydown', handleKeyPress);
